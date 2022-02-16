ae06f72d41add4bcb7b11ecaa05771198f4669c2
Sunakshi Mohanta <er.sunakshimohanta@gmail.com>
2022-02-16 15:50:20 +0530

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import BarChart from "./BarChart";
+import * as d3 from "d3";
+import "./App.css";
 
 function App() {
+  const [data, setData] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    d3.json("/chart-data.json").then((d) => {
+      setData(d);
+      setLoading(false);
+    });
+    return () => undefined;
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {loading && <div>loading</div>}
+        {!loading && <BarChart data={data} />}
       </header>
     </div>
   );
